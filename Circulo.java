efa8e3171e3ac73d86d03e9af48dc95f9cb743d2
alefortuny <122295472+alefortuny@users.noreply.github.com>
2023-02-10 19:53:37 -0600

    Add files via upload

---
+++ Circulo.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class Circulo 
+{
+
+	public float radio;
+	
+	public Circulo(float radio)
+	{
+		this.radio = radio;
+	}
+	public Circulo()
+	{
+		this(173913);
+	}
+	public double getCircunference() 
+	{
+		return radio*2*Math.PI;
+	}
+	public double getArea() 
+	{
+		return Math.PI*Math.pow(radio, 2);
+	}
+}
